eaed6bb1f96bfd10c2e76de782780090b7cc7d0d
mnansary <silentassasin0111@gmail.com>
2025-06-08 00:51:13 +0600

    full qna

--- 4.qna_extractor.py
+++ 4.qna_extractor.py
@@ -25,25 +25,34 @@ for topic,_df in df.groupby("topic"):
     json_data=[]
     os.makedirs(os.path.join(save_dir,topic),exist_ok=True)
     for subject,_sdf in _df.groupby("path"):
+        if os.path.exists(os.path.join(save_dir,topic,f"{subject}.json")):
+            continue
         print(f"Topic:{topic} Subject:{subject} fatwas:{len(_sdf)}")
         links=_sdf.fatwa.tolist()
         for link in tqdm(links):
-            driver.get(link)
-            question= driver.find_element(value=question_item, by="xpath")
-            question=question.find_element(value=".//div[@itemprop='text']", by="xpath")
-            question_text = question.text.replace("السؤال","").strip()
-            question_text = arabic_reshaper.reshape(question_text)
-            #print(f"Question: {question_text}")
-            answer= driver.find_element(value=answer_item, by="xpath")
-            answer=answer.find_element(value=".//div[@itemprop='text']", by="xpath")
-            answer_text = answer.text.replace("الإجابــة","").strip()
-            answer_text = arabic_reshaper.reshape(answer_text)
-            
-            #print(f"Answer: {answer_text}")
-            json_data.append({
-                "سؤال": question_text,
-                "إجابة": answer_text
-            })
+            try:
+                driver.get(link)
+                question= driver.find_element(value=question_item, by="xpath")
+                question=question.find_element(value=".//div[@itemprop='text']", by="xpath")
+                question_text = question.text.replace("السؤال","").strip()
+                question_text = arabic_reshaper.reshape(question_text)
+                #print(f"Question: {question_text}")
+                answer= driver.find_element(value=answer_item, by="xpath")
+                answer=answer.find_element(value=".//div[@itemprop='text']", by="xpath")
+                answer_text = answer.text.replace("الإجابــة","").strip()
+                answer_text = arabic_reshaper.reshape(answer_text)
+                
+                #print(f"Answer: {answer_text}")
+                json_data.append({
+                    "سؤال": question_text,
+                    "إجابة": answer_text
+                })
+            except Exception as e: 
+                print()
+                print(e)
+                print(link)
+                print()
+
         with open(os.path.join(save_dir,topic,f"{subject}.json") , "w", encoding="utf-8") as f:
             json.dump(json_data, f, ensure_ascii=False, indent=4)
         
